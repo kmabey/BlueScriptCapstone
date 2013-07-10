05a1a2195a1bb197f7c04771613715cdf9048977
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-10 15:28:25 -0600

    Database in CRUD project now working
    The Database now works

---
+++ CODE/CRUD/CRUD/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.DAL;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+    }
+}
