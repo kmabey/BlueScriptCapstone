158964940e3fbce8a9e2b761b02f64b82bb00aa0
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-12 13:35:33 -0600

    Form still in progress
    changed the way I am doing the form, should be easier to complete now.

--- CODE/MiniProject01/MiniProject01/Controllers/HomeController.cs
+++ CODE/MiniProject01/MiniProject01/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MiniProject01.Models;
 
 namespace MiniProject01.Controllers
 {
---
+++ CODE/MiniProject01/MiniProject01/Models/InformationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniProject01.Models
+{
+    public class InformationModel
+    {
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string input { get; set; }
+    }
+}
