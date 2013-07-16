77e431be9def4e94c8dd20a518c537d8508e1269
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-16 14:23:56 -0600

    Small changes to mockups
    started creating the skeleton of the blue script site itself. I will be
    removing the css and layouts to start from the bare basics

---
+++ CODE/BlueScript/BlueScript/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlueScript.Models
+{
+    public class Project
+    {
+        //This will be the Project model that holds all projects for the signed 
+        //in user. 
+    }
+}
