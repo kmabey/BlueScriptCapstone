3b5dcd99a151f229bb953d181be4570b2df2cfd0
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-20 18:27:55 -0600

    still fixing problems in code

--- CODE/Tester/Tester/assets/JavaScript/search.js
+++ CODE/Tester/Tester/assets/JavaScript/search.js
@@ -1,20 +1,8 @@
-/*
- * This is the function that actually highlights a text string by
- * adding HTML tags before and after all occurrences of the search
- * term. You can pass your own tags if you'd like, or if the
- * highlightStartTag or highlightEndTag parameters are omitted or
- * are empty strings then the default <font> tags will be used.
- */
 function doHighlight(bodyText, searchTerm) {
 
-    String highlightStartTag = "<font style='color:blue; background-color:yellow;'>";
-    String highlightEndTag = "</font>";
-  
-  // find all occurences of the search term in the given text,
-  // and add some "highlight" tags to them (we're not using a
-  // regular expression search, because we want to filter out
-  // matches that occur within HTML tags and script blocks, so
-  // we have to do a little extra validation)
+    var highlightStartTag = "<font style=color:blue; background-color:yellow;>";
+    var highlightEndTag = "</font>";
+
   var newText = "";
   var i = -1;
   var lcSearchTerm = searchTerm.toLowerCase();
@@ -32,19 +20,11 @@ function doHighlight(bodyText, searchTerm) {
           i = -1;
         }
       }
+        return newText;
     }
-  }
   
-  return newText;
-}
 
-/*
- * This is sort of a wrapper function to the doHighlight function.
- * It takes the searchText that you pass, optionally splits it into
- * separate words, and transforms the text on the current web page.
- * Only the "searchText" parameter is required; all other parameters
- * are optional and can be omitted.
- */
+
 function highlightSearchWord(searchText)
 {
   if (!document.body || typeof(document.body.innerHTML) == "undefined") {
@@ -62,13 +42,7 @@ function highlightSearchWord(searchText)
 }
 
 
-/*
- * This displays a dialog box that allows a user to enter their own
- * search terms to highlight on the page, and then passes the search
- * text or phrase to the highlightSearchTerms function. All parameters
- * are optional.
- */
-function searchPrompt(var defaultText)
+function searchPrompt(defaultText)
 {
     
     var highlightStartTag = "";
