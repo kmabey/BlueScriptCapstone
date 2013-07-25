733fa865d64973715cd4f233e58ea7028fdfed60
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-25 14:16:18 -0600

    Drag and Drop basics done.
    now working on dropping to delete.

--- CODE/Blue_Script/Blue_Script/assets/JavaScript/myjs.js
+++ CODE/Blue_Script/Blue_Script/assets/JavaScript/myjs.js
@@ -3,9 +3,14 @@
 
 });
 
+var dragSrcEl = null;
 function handleDragStart(e) 
 {
-    this.style.opacity = '0.4';  
+
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
 function handleDragOver(e) 
@@ -30,11 +35,60 @@ function handleDragLeave(e)
     this.classList.remove('over');
 }
 
-var cols = document.querySelectorAll('#event .event');
-[ ].forEach.call(cols, function (col) 
+function handleDrop(e) 
 {
-    col.addEventListener('dragstart', handleDragStart, false);
-    col.addEventListener('dragenter', handleDragEnter, false);
-    col.addEventListener('dragover', handleDragOver, false);
-    col.addEventListener('dragleave', handleDragLeave, false);
+    if (e.stopPropagation) 
+    {
+        e.stopPropagation();
+    }
+
+    if (dragSrcEl != this) 
+    {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+
+    return false;
+}
+
+function handleDragEnd(e) 
+{
+    [ ].forEach.call(events, function (ev) 
+    {
+        ev.classList.remove('over');
+    });
+}
+
+var events = document.querySelectorAll('[draggable=true]');
+[ ].forEach.call(events, function (ev) 
+{
+    ev.addEventListener('dragstart', handleDragStart, false);
+    ev.addEventListener('dragenter', handleDragEnter, false);
+    ev.addEventListener('dragover', handleDragOver, false);
+    ev.addEventListener('dragleave', handleDragLeave, false);
+    ev.addEventListener('drop', handleDrop, false);
+    ev.addEventListener('dragend', handleDragEnd, false);
 });
+
+var bin = document.getElementById('bin');
+bin.addEventListener('drop', function (e) 
+{
+    var file_name = e.dataTransfer.getData('Text'),
+        file = document.querySelector('.file[data-name="' + file_name + '"]');
+
+    //If the file exists...
+    if (file) 
+    {
+        //We change the message
+        document.getElementById('results').innerHTML = 'The file <em>' + file_name + '</em> has been deleted!';
+
+        //We remove the file from the DOM
+        file.parentNode.removeChild(file);
+
+        //We add the "full" class to the bin in order to change its background
+        addClass(this, 'full');
+    }
+
+    //And we prevent the default action!					
+    if (e.preventDefault) { e.preventDefault(); }
+}, false);
