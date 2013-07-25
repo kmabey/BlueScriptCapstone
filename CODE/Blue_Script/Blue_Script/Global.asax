cd6eaf2320870fe1ef21d5511a1b79d631e244d1
Kylie Mabey <kmabey@student.neumont.edu>
2013-07-25 13:04:10 -0600

    Drag almost finished

---
+++ CODE/Blue_Script/Blue_Script/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blue_Script.MvcApplication" Language="C#" %>
---
+++ CODE/Blue_Script/Blue_Script/assets/JavaScript/myjs.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () 
+{
+
+});
+
+function handleDragStart(e) 
+{
+    this.style.opacity = '0.4';  
+}
+
+function handleDragOver(e) 
+{
+    if (e.preventDefault) 
+    {
+        e.preventDefault(); 
+    }
+
+    e.dataTransfer.dropEffect = 'move';  
+
+    return false;
+}
+
+function handleDragEnter(e) 
+{
+    this.classList.add('over');
+}
+
+function handleDragLeave(e) 
+{
+    this.classList.remove('over');
+}
+
+var cols = document.querySelectorAll('#event .event');
+[ ].forEach.call(cols, function (col) 
+{
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false);
+    col.addEventListener('dragover', handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+});
